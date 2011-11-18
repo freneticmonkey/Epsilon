8c617374eceaa6dd6d7ad041ae3a2574e6c8104c
Scott Porter <scottporter@neuroticstudios.com>
2011-11-19 09:46:31 +1100

    Add: Placeholder for future shadow implementation.

---
+++ src/Render/Shadow.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 5, 2011
+
+@author: scottporter
+'''
+
+
+class Shadow(object):
+    def __init__(self):
+        pass
+    
+    def BeforeRender(self):
+        pass
+    
+    def AfterRender(self):
+        pass
+        
