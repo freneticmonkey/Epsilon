cf45bb957be5b9f14ca18fa7ad4b79e837470a4f
Scott Porter <scottporter@neuroticstudios.com>
2012-02-10 00:25:31 +1100

    Mod: Improved implementation of time

--- src/Core/Time.py
+++ src/Core/Time.py
@@ -7,31 +7,31 @@ from time import time, clock
 from datetime import datetime
 import pygame
 
+from Core.BaseManager import BaseSingleton
+
 MAX_FPS = 1000
 
-deltaTime = 0.00001
+delta_time = 0.000001
 
-#_frame_start = clock() 
-_frame_start = datetime.now()
-
-_pygame_clock = None
-
-def UpdateDelta():
-    global deltaTime
-    global _pygame_clock
-    if _pygame_clock is None:
-        _pygame_clock = pygame.time.Clock()
+class Time(BaseSingleton):
     
-#    global _frame_start
-#    el = datetime.now() - _frame_start
-#    deltaTime = el.microseconds / 1000000.0
-#    _frame_start = datetime.now()
-
-    deltaTime = _pygame_clock.tick(MAX_FPS) / 1000.0
+    def __init__(self):
+        self.deltaTime = 0.00001
+        self._frame_start = datetime.now()
+        self._pygame_clock = pygame.time.Clock()
+        
+    @property
+    def delta_time(self):
+        return self._delta_time
+        
+    def update_delta(self):
+        # This method is not ideal, but until I can figure out some kind of 
+        # class property...
+        global delta_time
+        self._delta_time = self._pygame_clock.tick(MAX_FPS) / 1000.0
+        delta_time = self._delta_time
+        
     
-def GetFPS():
-    global _pygame_clock
-    if _pygame_clock is None:
-        _pygame_clock = pygame.time.Clock()
-    
-    return _pygame_clock.get_fps()
+    @classmethod
+    def get_fps(cls):
+        return cls._instance._pygame_clock.get_fps()
