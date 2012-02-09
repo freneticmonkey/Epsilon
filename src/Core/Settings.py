83eb038baeee9b52cd111de97421c89416ac518c
Scott Porter <scottporter@neuroticstudios.com>
2012-02-10 00:13:50 +1100

    Add: Created a Settings module in which all of the engine settings can reside.

---
+++ src/Core/Settings.py
@@ -0,0 +1,18 @@
+'''
+Created on Feb 4, 2012
+
+@author: scottporter
+'''
+#from Geometry.euclid import Vector2
+
+class DisplaySettings:
+    resolution = [800, 600]
+    window_title = "Epsilon"
+    fullscreen = False
+    
+    
+class LoggerSettings:
+    method = 'file'
+    filename = 'EpsilonLog.txt'
+    log_to_console = True
+    
