5ea75895778c06e846c3aa833f24a2f550de9f99
Scott Porter <scottporter@neuroticstudios.com>
2012-02-10 00:31:08 +1100

    Mod: Moved mouse input processing from the Input class into the movement class.  Added support for focus sharing with UI

--- src/Scripting/TestScripts.py
+++ src/Scripting/TestScripts.py
@@ -2,23 +2,36 @@
 Created on Sep 20, 2011
 
 @author: scottporter
+
+
+NOTE: Considering giving scripts priority settings to move them within the
+      ScriptManager's array -> higher priority = further towards the start
+      of the array
+
 '''
+
+
+
 from Core import Time
+from Core import Settings
+from Events.EventBase import EventBase
+from Events.ListenerBase import ListenerBase
 from Scripting.Script import Script
 from Geometry.euclid import Vector3, Quaternion
-from Render import RenderCore
+from Render.RenderManager import RenderManager
 
 from pygame import *
 
 import math
 
-from Core import Input
+from Core.Input import Input
 
 class RotateScript(Script):
     
-    def __init__(self, parent_node=None, rate=15):
+    def __init__(self, parent_node=None, rate=15, axis=Vector3(0,1,0)):
         Script.__init__(self, 'RotateScript', parent_node)
         self._rate = rate
+        self._axis = axis
     
     def Init(self):
         pass
@@ -27,71 +40,118 @@ class RotateScript(Script):
         pass
             
     def Update(self):
-        up = Vector3(0,1,0)
         angle = self._rate * (math.pi/180)
-        angle_inc = angle * Time.deltaTime
+        angle_inc = angle * Time.delta_time
 #        rot = self._node.transform.local_rotation
         if self._node:
-            self._node.rotate( Quaternion().new_rotate_axis(angle_inc, up) )
+            self._node.rotate( Quaternion().new_rotate_axis(angle_inc, self._axis) )
         
 #        self._node.translate( Vector3(1*Time.deltaTime,0,0))
         
     def Shutdown(self):
         pass
             
-class MoveController(Script):
-    
+class MoveController(Script, ListenerBase):
+    event_types = ['MouseEnterUI','MouseExitUI']
     def __init__(self, parent_node=None,speed=10,angle_speed=120):
-        Script.__init__(self, 'MoveController', parent_node)
+        Script.__init__(self, name='MoveController', parent_node=parent_node)
+        ListenerBase.__init__(self, eventTypes=self.event_types)
         self._speed = speed
         self._angle_speed = angle_speed
+        
+        self._initial_mouse_down = False
+        self._initial_mouse_up = False
+        self._res = Settings.DisplaySettings.resolution
+        self._h_res = [self._res[0]/2, self._res[1]/2]
+        mouse.set_visible(True)
+        
+        self._mouse_over_ui = False
+    
+    def _processEvent(self, new_event):
+        if new_event.name == 'MouseEnterUI':
+            self._mouse_over_ui = True
+        elif new_event.name == 'MouseExitUI':
+            self._mouse_over_ui = False
     
     def Update(self):
         
-        if Input.GetKey(K_a):
-            self._node.translate(  ( Vector3.RIGHT() * self._speed * Time.deltaTime ) )
-        if Input.GetKey(K_d):
-            self._node.translate(  ( Vector3.RIGHT() * -self._speed * Time.deltaTime ) )
-        if Input.GetKey(K_w):
-            self._node.translate( ( Vector3.FORWARD() * self._speed * Time.deltaTime ) )
-        if Input.GetKey(K_s):
-            self._node.translate( ( Vector3.FORWARD() * -self._speed * Time.deltaTime ) )
+        if not self._mouse_over_ui:
+            # If the left mouse button is down
+            mouse_left_down = Input.get_mouse_left()
             
-        if Input.GetKey(K_r):
-            self._node.translate( ( Vector3.UP() * self._speed * Time.deltaTime ) )
-        if Input.GetKey(K_f):
-            self._node.translate( ( Vector3.UP() * -self._speed * Time.deltaTime ) )
-        
-        angle = self._angle_speed * Time.deltaTime
-        angle = angle * (math.pi / 180 )
-#        rot = self._node.rotation
-        if Input.GetKey(K_q):
-            self._node.rotate(Quaternion().rotate_axis(angle, Vector3(0,1,0)) )
-            
-        if Input.GetKey(K_e):
-            self._node.rotate( Quaternion().rotate_axis(-angle, Vector3(0,1,0)) )
+            # If the left mouse is down 
+            if mouse_left_down:
+                # Hide the cursor
+                mouse.set_visible(False)
+                
+                # And its not the initial frame
+                if not self._initial_mouse_down:
+                    # Reset the mouse position to the centre of the screen
+                    self._mouse_x, self._mouse_y = mouse.get_pos()
+                    self._mouse_x -= self._h_res[0]
+                    self._mouse_y -= self._h_res[1]
+                    Input.set_mouse_pos(self._mouse_x, self._mouse_y)
+                else:
+                    self._initial_mouse_down = False
+                    Input.set_mouse_pos(0, 0)
+                mouse.set_pos(self._h_res[0],self._h_res[1])
+                self._initial_mouse_up = True
+                
+                # Handle Keyboard
+                if Input.get_key(K_a):
+                    self._node.translate(  ( Vector3.RIGHT() * self._speed * Time.delta_time ) )
+                if Input.get_key(K_d):
+                    self._node.translate(  ( Vector3.RIGHT() * -self._speed * Time.delta_time ) )
+                if Input.get_key(K_w):
+                    self._node.translate( ( Vector3.FORWARD() * self._speed * Time.delta_time ) )
+                if Input.get_key(K_s):
+                    self._node.translate( ( Vector3.FORWARD() * -self._speed * Time.delta_time ) )
+                    
+                if Input.get_key(K_r):
+                    self._node.translate( ( Vector3.UP() * self._speed * Time.delta_time ) )
+                if Input.get_key(K_f):
+                    self._node.translate( ( Vector3.UP() * -self._speed * Time.delta_time ) )
+                
+                angle = self._angle_speed * Time.delta_time
+                angle = angle * (math.pi / 180 )
+        #        rot = self._node.rotation
+                if Input.get_key(K_q):
+                    self._node.rotate(Quaternion().rotate_axis(angle, Vector3(0,1,0)) )
+                    
+                if Input.get_key(K_e):
+                    self._node.rotate( Quaternion().rotate_axis(-angle, Vector3(0,1,0)) )
+                
+            else:
+                self._initial_mouse_down = True
+                if self._initial_mouse_up:
+                    # Show the cursor
+                    mouse.set_visible(True)
+                    self._initial_mouse_up = False
             
 class CameraMoveController(MoveController):
+    def __init__(self, parent_node=None,speed=10,angle_speed=120):
+        super(CameraMoveController, self).__init__(parent_node,speed,angle_speed)
     
     def Update(self):
+        super(CameraMoveController, self).Update()
         
-        pos = self._node.position
-        forward = self._node.forward
-        
-        # Mouse Controls
-        angle = self._angle_speed * (math.pi / 180)
-        angle = 0.002
-        mx, my = Input.GetMouseMove()
-        h_angle = angle * -mx
-        v_angle = angle * my
-        self._node.rotate(Quaternion().rotate_axis(h_angle, Vector3(0,1,0)) )
-        self._node.rotate(Quaternion().rotate_axis(v_angle, Vector3(1,0,0)) )
-        
-        
-        if hasattr( self._node, 'LookAt'):
-            self._node.look_at = pos + ( forward * self._speed)
+        if Input.get_mouse_left() and not self._mouse_over_ui:
+            pos = self._node.position
+            forward = self._node.forward
             
-        MoveController.Update(self)
+            # Mouse Controls
+    #        angle = self._angle_speed * (math.pi / 180)
+            angle = 0.002
+            mx, my = Input.get_mouse_move()
+            h_angle = angle * -mx
+            v_angle = angle * my
+            self._node.rotate(Quaternion().rotate_axis(h_angle, Vector3(0,1,0)) )
+            self._node.rotate(Quaternion().rotate_axis(v_angle, Vector3(1,0,0)) )
+            
+            
+            if hasattr( self._node, 'LookAt'):
+                self._node.look_at = pos + ( forward * self._speed)
+            
             
 class DisplayCoordinate(Script):
     
@@ -104,22 +164,33 @@ class DisplayCoordinate(Script):
 #        if not self._node.transform.position == self._last_pos:
             self._last_pos = self._node.position 
             print self._node.name + ": " + str(self._last_pos)
-            
+
+class QuitEvent(EventBase):
+    def __init__(self):
+        EventBase.__init__(self, 'Quit', True)
+    
 class SettingsController(Script):
     
     def __init__(self, parent_node=None):
         Script.__init__(self, 'SettingsController', parent_node)
         self._wireframe = False
         self._grid = True 
-        self._renderer = RenderCore.GetRenderCore()._renderer
+        self._renderer = RenderManager.get_instance()._renderer
+        self._quit_detected = False
     
     def Update(self):
         
-        if Input.GetKeyDown(K_0):
+        if Input.get_key_down(K_0):
             self._wireframe = not self._wireframe
             self._renderer.wireframe = self._wireframe
         
-        if Input.GetKeyDown(K_9):
+        if Input.get_key_down(K_9):
             self._grid = not self._grid
             self._renderer.grid = self._grid
+            
+        if Input.get_key_down(K_ESCAPE) or Input.get_quit_detected():
+            # Fire a Quit Event
+            QuitEvent().Send()
+            
+        
     
