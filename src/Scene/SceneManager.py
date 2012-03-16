34fc48d71648d62bd25469337de010db4481a198
Scott Porter <scottporter@neuroticstudios.com>
2012-03-16 22:51:50 +1100

    Add: Added actual Scenes to the SceneManager as part of ResourceManager implementation

--- src/Scene/SceneManager.py
+++ src/Scene/SceneManager.py
@@ -25,80 +25,46 @@ class SceneManagerEvents:
 class SceneManager(BaseSingleton):
     
     def __init__(self):
-        self._root = None
-        self._cameras = []
+        #self._root = None
+        #self._cameras = []
+        #self._lights = []
         self._active_camera = None
+        self._scenes = []
+        self._current_scene = None
         
-        self._lights = []
+    def init(self):
+#        self._root = Node(name='scene_root')
+#        self._scene_listener = SceneManagerListener()
+        pass
+    
+#    @property
+#    def root(self):
+#        return self._root
+    
+    def add_scene(self, new_scene):
+        if not len(self._scenes):
+            self._current_scene = new_scene
+            new_scene.active = True
+        self._scenes.append(new_scene)
         
-    def Init(self):
-        self._root = Node(name='scene_root')
-        self._scene_listener = SceneManagerListener()
-        
+    def remove_scene(self, del_scene):
+        if del_scene == self._current_scene:
+            self._current_scene.active = False
+            self._current_scene = None
+        self._scenes.remove(del_scene)
+
     @property
-    def root(self):
-        return self._root
+    def scenes(self):
+        return self._scenes
+    
+    @property
+    def current_scene(self):
+        return self._current_scene
     
     def Update(self):
-        if self._root._need_child_update or \
-           self._root._need_parent_update or \
-           len(self._root._children_to_update) > 0:
-            # Update the Transform Nodes within the Scene
-            self._root._Update(True, False)
+        if not self._current_scene is None and self._current_scene.need_update:
+            self._current_scene.update()
     
-    @property
-    def cameras(self):
-        return self._cameras
-    
-    @property
-    def active_camera(self):
-        return self._active_camera
-    
-    @property
-    def lights(self):
-        return self._lights
-    
-    # Add the camera parameter to the list of known cameras in the scene
-    def AddCamera(self, new_camera):
-        # If the camera isn't already on the list of cameras
-        if not new_camera in self._cameras:
-            self._cameras.append(new_camera)
-            # If the camera is the only camera listed, it becomes the
-            # active camera by default
-            if len(self._cameras) == 1:
-                self._active_camera = self._cameras[0]
-            
-    # Remove the camera param from the list of known cameras in the scene.
-    def RemoveCamera(self, rm_camera):
-        if rm_camera in self._cameras:
-            self._cameras.remove(rm_camera)
-            
-            # If there is only a single camera in the scene, it becomes the
-            # active camera by default
-            self._active_camera = self._cameras[0]
-            
-    # Set the camera parameter as the scenes active camera
-    def SetActiveCamera(self, active_camera):
-        # If the camera is amongst the cameras that the SceneManager is aware of
-        if active_camera in self._cameras:
-            # Set all other cameras to be inactive
-            for camera in self._cameras:
-                camera.active = (camera==active_camera)
-            # Set the SceneManagers active camera to be the parameter
-            self._active_camera = active_camera       
-            
-    # Add the light parameter to the list of known lights in the scene
-    def AddLight(self, new_light):
-        # If the light isn't already on the list of lights
-        if not new_light in self._lights:
-            self._lights.append(new_light)
-            
-    # Remove the light param from the list of known lights in the scene.
-    def RemoveLight(self, rm_light):
-        if rm_light in self._lights:
-            self._lights.remove(rm_light)
-    
-
 class SceneManagerListener(ListenerBase):
 
     def __init__(self):
