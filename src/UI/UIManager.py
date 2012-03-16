00b2779ef10526f29c6903ec847678ee9bb5e216
Scott Porter <scottporter@neuroticstudios.com>
2012-03-16 22:42:46 +1100

    Add: Added a python console to the UI

--- src/UI/UIManager.py
+++ src/UI/UIManager.py
@@ -15,9 +15,22 @@ class UIManager(FrameListenerManager):
         # Initialise GUI Sub-system
         pyui.init(DisplaySettings.resolution[0],DisplaySettings.resolution[1], "p3d", DisplaySettings.fullscreen)
             
-        # Create a default window
-        #frame = pyui.widgets.Frame(10,10,200,200, "hello world")#, theme=self._desktop.theme)
+        # A python console
         self._console = pyui.dialogs.Console(10,10,400,400)
+        
+        # Stats dialog
+        height = 100
+        buffer = 10
+        width = 200
+        x = buffer
+        y = DisplaySettings.resolution[1]-(height+buffer)
+        # Position at the bottom of the screen
+        #self._stats = pyui.widgets.Frame(x,y,width,height, "Render Stats")
+        #self._stats = pyui.dialogs.StdDialog("Render Stats:", "Some stats here")#x,y,width,height, "Render Stats")
+        
+    @property
+    def stats_dialog(self):
+        return self._stats
     
     def draw(self):
         pyui.draw()
