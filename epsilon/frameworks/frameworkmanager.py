2f42153116fe3bdc158473ac13677019a572d680
Scott Porter <scottporter@neuroticstudios.com>
2012-08-04 16:11:28 +1000

    Mod: Moved the import of the frameworks into the constructor
    This was done so that PyGame or Pyglet could be used without having to
    install the other.

--- epsilon/frameworks/frameworkmanager.py
+++ epsilon/frameworks/frameworkmanager.py
@@ -7,17 +7,16 @@ Created on Apr 26, 2012
 from epsilon.core.settings import Frameworks, FrameworkSettings
 from epsilon.core.basesingleton import BaseSingleton
 
-from epsilon.frameworks.pygameframework import PyGameFramework
-from epsilon.frameworks.pygletframework import PygletFramework
-
 class FrameworkManager(BaseSingleton):
     def __init__(self):
         self._framework = None
         
         if FrameworkSettings.use_framework == Frameworks.PYGAME:
+            from epsilon.frameworks.pygameframework import PyGameFramework
             self._framework = PyGameFramework.get_instance()
             
         elif FrameworkSettings.use_framework == Frameworks.PYGLET:
+            from epsilon.frameworks.pygletframework import PygletFramework
             self._framework = PygletFramework.get_instance()
             
     @property
