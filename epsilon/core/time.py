a0257ee5aca3561ca8b5aaaaddfefbbbbee227b6
Scott Porter <scottporter@neuroticstudios.com>
2012-08-04 16:06:07 +1000

    Mod: Improved implementation of Time class
    Improved storage and access of delta_time directly from the Time class

--- epsilon/core/time.py
+++ epsilon/core/time.py
@@ -16,26 +16,31 @@ from epsilon.core.basemanager import BaseSingleton
 
 class Time(BaseSingleton):
     delta_time = 0
+    SMOOTH_A = 0.9
+    SMOOTH_B = 0.1
     
     def __init__(self):
-        self.deltaTime = 0.00001
         self._last_frame_start = datetime.now()
+        self._delta_time = 0.00001
+        self._last_delta_time = 0.0001
+        self._fps = 0.0
 #        self._last_frame_start = datetime.now()
         #self._pygame_clock = pygame.time.Clock()
         
 #    @property
 #    def delta_time(self):
 #        return self._delta_time
-        
+    
     def update_delta(self):
         # This method is not ideal, but until I can figure out some kind of 
         # class property...
         
         #self._delta_time = self._pygame_clock.tick(MAX_FPS) / 1000.0
         now = datetime.now()
-        self.__class__.delta_time = (now - self._last_frame_start).total_seconds()
-        self._last_frame_start = now       
-    
-#    @classmethod
-#    def get_fps(cls):
-#        return cls._instance._pygame_clock.get_fps()
+        self._delta_time = (now - self._last_frame_start).total_seconds()
+        self._last_frame_start = now
+        self.__class__.delta_time = self._delta_time
+        
+        self._fps = 1000 / ( (self._delta_time * self.SMOOTH_A + self._last_delta_time + self.SMOOTH_B) * 100)
+        self.__class__.fps = self._fps
+        
