737c14772518b28422ca7461dbf409ea9ff9598d
Scott Porter <scottporter@neuroticstudios.com>
2012-08-04 16:10:10 +1000

    Mod: Cleaned up the PyGame framework implementation
    Removed a lot of the guff from the implementation.  Also improved the
    mouse handling so that the mouse doesn't always re-enter when exclusive
    mode is enabled.

--- epsilon/frameworks/pygameframework.py
+++ epsilon/frameworks/pygameframework.py
@@ -6,7 +6,7 @@ Created on Apr 26, 2012
 import pygame
 from pygame import mouse 
 
-from epsilon.logging import Logger
+from epsilon.logging.logger import Logger
 from epsilon.frameworks.baseframework import BaseFramework
 
 from epsilon.core.input import Input
@@ -29,9 +29,10 @@ class PyGameFramework(BaseFramework):
         # Configure Window
         pygame.display.set_mode((self._width, self._height), pygame.OPENGL|pygame.DOUBLEBUF)
         pygame.display.set_caption(self._title)
-        Logger.Log("Initialised PyGame Window")
         
         self._input = PyGameInput()
+        
+        Logger.Log("Initialised PyGame Window")
         
     def start(self):
         self._setup()
@@ -53,21 +54,15 @@ class PyGameInput(Input):
         Input._instance = self
     
     def _framework_init(self):
-        self._key_events = []
-        
-        # Configure exclusive mouse properties
-        self._initial_mouse_down = False
-        self._initial_mouse_up = False
-        self._res = Settings.DisplaySettings.resolution
-        self._h_res = [self._res[0]/2, self._res[1]/2]
-        
-        self._init_mouse_x = 0
-        self._init_mouse_y = 0
+#        self._key_events = []
         
         self._exclusive_mouse = False
         self._mouse_rel_x = 0.0
         self._mouse_rel_y = 0.0
-        self._mouse_moved = False
+        
+        # Mouse Down position used for exclusive mode
+        self._ex_mouse_x = 0 
+        self._ex_mouse_y = 0
     
     @classmethod
     def get_key_events(cls):
@@ -85,19 +80,8 @@ class PyGameInput(Input):
     def _process_input(self):
 
         # Mouse
-        
-        # TODO: Hmm relative mouse movement is busted here. This needs to be fixed. Too tired atm.
-        #       Look at PygletInput for ideas. 
-        
-        # Set mouse position
-        if self._exclusive_mouse:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            self._mouse_rel_x = mouse_x - self._init_mouse_x
-            self._mouse_rel_y = mouse_y - self._init_mouse_y
-        else:
-            self._mouse_x, self._mouse_y = pygame.mouse.get_pos()
-            self._mouse_rel_x = 0.0
-            self._mouse_rel_y = 0.0
+        self._mouse_x, self._mouse_y = pygame.mouse.get_pos()
+        self._mouse_rel_x, self._mouse_rel_y = pygame.mouse.get_rel()
         
         mouse_buttons = pygame.mouse.get_pressed()
         
@@ -106,11 +90,12 @@ class PyGameInput(Input):
             state = mouse_buttons[button]
             
             self._process_input_state(button, state)
+            
         # Keyboard
         keys = pygame.key.get_pressed()
                 
         # Check for Keys pressed this frame
-        for key in range(0, len(keys)):
+        for key in range(3, len(keys)):
             
             state = keys[key]
             
@@ -121,50 +106,29 @@ class PyGameInput(Input):
         # Clear on each frame
         self._key_events = []
         
-        # Store the pygame key events for pyui access        
+#        # Store the pygame key events for pyui access        
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                self._key_events.append(event)
-            elif event.type == pygame.KEYUP:
-                self._key_events.append(event)
-            elif event.type == pygame.QUIT:
+#            if event.type == pygame.KEYDOWN:
+#                self._key_events.append(event)
+#            elif event.type == pygame.KEYUP:
+#                self._key_events.append(event)
+            if event.type == pygame.QUIT:
                 self._quit_detected = True
                 
     def set_exclusive_mouse(self, exclusive):
         
-        self._exclusive_mouse = exclusive
-        
-        if exclusive:
-            mouse.set_visible(False)
+        if not exclusive == self._exclusive_mouse:
+            self._exclusive_mouse = exclusive
             
-            # And its not the initial frame
-            if not self._initial_mouse_down:
-                # Reset the mouse position to the centre of the screen
-                #self._mouse_x, self._mouse_y = mouse.get_pos()
+            if self._exclusive_mouse:
+                mouse.set_visible(False)
+                pygame.event.set_grab(True)
                 
-#                self._mouse_x, self._mouse_y = self.get_mouse_move()
-                self._init_mouse_x = self._h_res[0]
-                self._init_mouse_y = self._h_res[1]
-                
-#                self._mouse_x -= self._h_res[0]
-#                self._mouse_y -= self._h_res[1]
-#                Input.set_mouse_pos(self._mouse_x, self._mouse_y)
+                self._ex_mouse_x, self._ex_mouse_y = pygame.mouse.get_pos()
             else:
-                self._initial_mouse_down = False
-#                Input.set_mouse_pos(0, 0)
-#                self._mouse_x = self._init_mouse_x
-#                self._mouse_y = self._init_mouse_x
-                
-            #mouse.set_pos(self._h_res[0],self._h_res[1])
-#            Input.set_mouse_pos(self._h_res[0],self._h_res[1])
-            self._mouse_x = self._init_mouse_x
-            self._mouse_y = self._init_mouse_y
-            
-            self._initial_mouse_up = True
-            
-        else:
-            self._initial_mouse_down = True
-            if self._initial_mouse_up:
                 # Show the cursor
                 mouse.set_visible(True)
-                self._initial_mouse_up = False
+                pygame.event.set_grab(False)
+                pygame.mouse.set_pos(self._ex_mouse_x, self._ex_mouse_y)
+            
+        
