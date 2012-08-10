bbae794b3aaffa7aafec1890d628777f9f4bc7d3
Scott Porter <scottporter@neuroticstudios.com>
2012-08-10 23:11:51 +1000

    Mod: Added imports for Textures, UI improvements, planet code and gizmo testing

--- epsilon/core/epsilonmanager.py
+++ epsilon/core/epsilonmanager.py
@@ -1,4 +1,4 @@
-from epsilon.logging import Logger
+from epsilon.logging.logger import Logger
 
 from epsilon.events.eventmanager import EventManager
 from epsilon.events.eventbase import EventBase
@@ -7,10 +7,13 @@ from epsilon.render.rendermanager import RenderManager
 from epsilon.scene.scenemanager import SceneManager  
 from epsilon.scripting.scriptmanager import ScriptManager
 from epsilon.ui.uimanager import UIManager
-from epsilon.render.texturemanager import TextureManager
+from epsilon.ui.ui import MainUI
+from epsilon.render.texturemanager import TextureManager # Disabled until textures are implemented in Pyglet
 from epsilon.render.shadermanager import ShaderManager
 from epsilon.render.shaders.phong import PhongShader
 from epsilon.render.shaders.phongsimple import PhongSimple
+from epsilon.render.gizmos.gizmomanager import GizmoManager
+from epsilon.render.gizmos.gizmos import WirePlane
 
 from epsilon.frameworks.frameworkmanager import FrameworkManager
 
@@ -21,6 +24,13 @@ from epsilon.resource.sceneresourcehandler import SceneResourceHandler
 from epsilon.resource.wavefrontresourcehandler import WavefrontResourceHandler
 
 from epsilon.core.coreevents import CoreListener
+
+# Terrain testing
+from epsilon.environment.terrain import Terrain
+from epsilon.environment.planet.planetsphere import PlanetSphere
+
+from epsilon.render.gizmos.gizmos import WireCube
+from epsilon.geometry.euclid import Vector3
 
 #from Render.Camera import CameraGL
 #from Render.Light import GLLight
@@ -38,6 +48,9 @@ from epsilon.core.settings import *
 #                                  CameraMoveController,  
 #                                  DisplayCoordinate, 
 #                                  SettingsController )
+
+from epsilon.render.meshfactory import MeshFactory, MeshTypes
+from epsilon.scene.node import Node
 
 ## Central Class to the Epsilon System
 
@@ -103,8 +116,12 @@ class EpsilonManager(object):
         # Start Scripting System
         self._script_manager = ScriptManager.get_instance()
         
-        # Start the UI Manager
-        self._ui_manager = UIManager.get_instance()
+        # Start the UI Manager and setup the MainUI
+#        self._ui_manager = UIManager.get_instance()
+#        self._main_ui = MainUI()
+        
+        # Start the GizmoManager system
+        self._gizmo_manager = GizmoManager.get_instance()
         
         # Configure the ResourceManager
         self._resource_manager = ResourceManager.get_instance()
@@ -125,6 +142,32 @@ class EpsilonManager(object):
         # Testing loading using the ResourceManager
         self._resource_manager.process_resource("scene.xml")
         #self._resource_manager.process_resource("empty_scene.xml")
+        
+        # Create a terrain node
+#        self._terrain = Terrain()
+#        
+#        # Generate the terrain mesh
+#        self._terrain.generate()
+#        
+#        self._terrain.local_scale.x = 20
+#        self._terrain.local_scale.z = 20
+#        
+#        self._scene.current_scene.root.add_child(self._terrain)
+    
+#        self._camera = self._scene.current_scene.active_camera
+#        
+#        wp = GizmoManager.draw_plane(Vector3(), Vector3.UP())
+        
+        #self._cube = WireCube(min=Vector3(-0.71,-1,-0.71),max=Vector3(0.71,1,0.71))
+        #self._scene.current_scene.root.transform.add_child(self._cube.transform)
+        
+#        self._planet = PlanetSphere()
+#        self._scene.current_scene.root.transform.add_child(self._planet.transform)
+        
+#        self._plane = Node()
+#        self._plane.renderer.mesh = MeshFactory.get_mesh(MeshTypes.OCTOHEDRON)
+#        self._plane.transform.position.y = 2.0
+#        self._scene.current_scene.root.transform.add_child(self._plane)
         
     def run(self):
         self._framework.start()
@@ -156,6 +199,8 @@ class EpsilonManager(object):
         
         # Update SceneManager
         self._scene.update()
+        
+#        self._planet.update_sphere()
         
         self._frame_end.send()
         
