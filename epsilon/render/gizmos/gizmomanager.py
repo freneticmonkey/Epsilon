231ea4c7ca949d66d3c5599b42bf09b6649706f0
Scott Porter <scottporter@neuroticstudios.com>
2012-09-18 21:22:25 +1000

    Mod: Made add/remove gizmo are now internal functions

--- epsilon/render/gizmos/gizmomanager.py
+++ epsilon/render/gizmos/gizmomanager.py
@@ -2,48 +2,54 @@
 from epsilon.core.basemanager import BaseSingleton
 from epsilon.geometry.euclid import Vector3
 from epsilon.render.gizmos.gizmos import WireSphere, WireCube, WirePlane, Line
+from epsilon.render.colour import Preset
 from epsilon.scene.node import Node
 
 class GizmoManager(BaseSingleton):
     
     available_gizmos = {'WireSphere' : WireSphere,
                         'WireCube' : WireCube,
-                        'Line' : Line
+                        'Line' : Line,
+                        'WirePlane' : WirePlane
                         }
     
     @classmethod
     def draw_sphere(cls, pos, radius=1.0):
         new_sphere = WireSphere(pos, radius)
-        cls.get_instance().add_gizmo(new_sphere)
+        cls.get_instance()._add_gizmo(new_sphere)
         
         return new_sphere
     
     @classmethod
-    def draw_cube(cls, pos, width=None, height=None, depth=None, max=Vector3(), min=Vector3()):
-        new_cube = WireCube(pos, width, height, depth, max, min)
-        cls.get_instance().add_gizmo(new_cube)
+    def draw_cube(cls, pos, width=None, height=None, depth=None, max=Vector3(), min=Vector3(), colour=Preset.white):
+        new_cube = WireCube(pos, width, height, depth, max, min, colour)
+        cls.get_instance()._add_gizmo(new_cube)
         return new_cube
     
     @classmethod
-    def draw_line(cls, pos, dir=Vector3(), length=1.0):
-        new_line = Line(pos, dir, length)
-        cls.get_instance().add_gizmo(new_line)
+    def draw_line(cls, pos, dir=Vector3(), length=1.0, colour=None):
+        new_line = Line(pos, dir, length, colour)
+        cls.get_instance()._add_gizmo(new_line)
         return new_line
     
     @classmethod
-    def draw_plane(cls, pos, normal=Vector3.UP(), size=1.0):
-        new_plane = WirePlane(pos, normal, size)
-        cls.get_instance().add_gizmo(new_plane)
+    def draw_plane(cls, pos, normal=Vector3.UP(), size=1.0, colour=Preset.white):
+        new_plane = WirePlane(pos, normal, size, colour)
+        cls.get_instance()._add_gizmo(new_plane)
         return new_plane
+
+    @classmethod
+    def remove_gizmo(cls, del_gizmo):
+        cls.get_instance()._remove_gizmo(del_gizmo)
     
     def __init__(self):
         self._gizmo_root = Node('gizmo_root')
         self._gizmo_root.transform.scale = Vector3(0.5, 0.5, 0.5)
         
-    def add_gizmo(self, new_gizmo):
+    def _add_gizmo(self, new_gizmo):
         self._gizmo_root.transform.add_child(new_gizmo.transform)
         
-    def remove_gizmo(self, del_gizmo):
+    def _remove_gizmo(self, del_gizmo):
         self._gizmo_root.transform.remove_child(del_gizmo.transform)
             
     def draw(self):
