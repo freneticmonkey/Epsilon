a0017c0613d5f2332c367ee8841a43444c7ff068
Scott Porter <scottporter@neuroticstudios.com>
2013-02-01 21:06:10 +1100

    Mod: Updated Colour class for OpenGL compatibility

--- epsilon/render/colour.py
+++ epsilon/render/colour.py
@@ -9,8 +9,8 @@ from OpenGL.GL import GLfloat
 
 from epsilon.render.glutilities import *
 
-class Colour( namedtuple('Colour','r g b a')):
-    
+class Colour(object):
+
     @classmethod
     def from_string(cls,colour_string):
         rgba = colour_string.split(" ")
@@ -20,6 +20,60 @@ class Colour( namedtuple('Colour','r g b a')):
         else:
             print "Colour: Invalid string parameter: " + colour_string
             return None
+
+    def __init__(self, r=1.0, g=1.0, b=1.0, a=1.0):
+
+        # Ensure internal storage is in artimetic format
+        if (r+g+b+a) > 4.0:
+            r /= 255.0
+            g /= 255.0
+            b /= 255.0
+            a /= 255.0
+
+        self._r = r
+        self._g = g
+        self._b = b
+        self._a = a
+
+    @property
+    def r(self):
+        return self._r
+
+    @r.setter
+    def r(self, new_value):
+        if new_value > 1.0:
+            new_value /= 255.0
+        self._r = new_value
+
+    @property
+    def g(self):
+        return self._g
+
+    @g.setter
+    def g(self, new_value):
+        if new_value > 1.0:
+            new_value /= 255.0
+        self._g = new_value
+
+    @property
+    def b(self):
+        return self._b
+
+    @b.setter
+    def b(self, new_value):
+        if new_value > 1.0:
+            new_value /= 255.0
+        self._b = new_value
+
+    @property
+    def a(self):
+        return self._a
+
+    @a.setter
+    def a(self, new_value):
+        if new_value > 1.0:
+            new_value /= 255.0
+        self._a = new_value
     
     def __repr__(self):
         return 'Colour(%f, %f, %f, %f)' % (self.r, self.g, self.b, self.a)
@@ -33,7 +87,6 @@ class Colour( namedtuple('Colour','r g b a')):
     # Get a Colour object with a random colour
     @staticmethod
     def random():
-        #return Colour( randint(0,255), randint(0,255), randint(0,255), 255 )
         return Colour( random(), random(), random(), 1.0)
     
     def inverted(self):
@@ -47,23 +100,27 @@ class Colour( namedtuple('Colour','r g b a')):
         if other is None:
             other = Preset.white
         
-        return Colour( int(self.r * (1 - proportion) + other.r * proportion),
-                       int(self.g * (1 - proportion) + other.g * proportion),
-                       int(self.b * (1 - proportion) + other.b * proportion),
-                       int(self.a * (1 - proportion) + other.a * proportion)                      
+        return Colour( self.r * (1 - proportion) + other.r * proportion,
+                       self.g * (1 - proportion) + other.g * proportion,
+                       self.b * (1 - proportion) + other.b * proportion,
+                       self.a * (1 - proportion) + other.a * proportion                      
                      )
     # Returns the colour as an array in the range of 0 - 1.0
-    def get_float_colour(self):   
-        return (self.r / 255.0), (self.g / 255.0), (self.b / 255.0), (self.a / 255.0)
+    def get_arithmetic(self):
+        return (self.r, self.g, self.b, self.a)
     
+    # Returns the colour as an array in the range 0 - 255
+    def get_8bit(self):
+        return ( self.r * 255, self.g * 255.0, self.b * 255.0, self.a * 255.0)
     
     # Get OpenGL Array representation of the Colour
     def get_gl_colour(self):
-        return CreateGLArray(GLfloat, self, 4)
+        return CreateGLArray(GLfloat, self.get_8bit(), 4)
     
     # Get a normalised array representation of the colour
     def get_shader_colour(self):
-        return ((self.r/256.0), (self.g/256.0), (self.b/256.0), self.a)
+        #return ((self.r/255.0), (self.g/255.0), (self.b/255.0), self.a)
+        return self.get_arithmetic()
     
 class Preset:
     # Preset Colours
