f224efee9823121937bd1d2731fa94e9d876450f
scott <scottporter@neuroticstudios.com>
2013-02-09 15:11:25 +1100

    Mod: Updated phong shader to use new helper functions in Shader class

--- epsilon/render/shaders/phongsimple.py
+++ epsilon/render/shaders/phongsimple.py
@@ -8,9 +8,7 @@ import os
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-from epsilon.logging.logger import Logger
-
-from epsilon.render.shader import ShaderProgram, VertexShader, FragmentShader
+from epsilon.render.shader import ShaderProgram, VertexShader, FragmentShader#, GeometryShader
 from epsilon.render.colour import Colour
 
 class PhongSimple(ShaderProgram):
@@ -30,97 +28,36 @@ class PhongSimple(ShaderProgram):
                                 'material.shininess',
                                 'diffuse_texture'
                               ]
-        self._uniform_locations = {}
+        self._attribute_names = ['Vertex_position',
+                                 'Vertex_normal',
+                                 'Vertex_texture_coordinate']
         
-        self._vertex_position_attr_location = None
-        self._vertex_normal_attr_location = None
-        self._vertex_texture_coord_attr_location = None
-        
-    def _configure_properties(self):
-        # Get the Memory locations of the Shader Uniforms
-        for uniform_name in self._uniform_names:
-            location = glGetUniformLocation(self._id, uniform_name)
-            
-            if not location in [None,-1]:
-                self._uniform_locations[uniform_name] = location
-            else:
-                Logger.Log("Shader error: uniform not found: " + uniform_name)
-        
-        # Get the Memory locations of the Shader Attributes
-        location = glGetAttribLocation(self._id, "Vertex_position")
-        if not location in [None,-1]:
-                self._vertex_position_attr_location = location
-        else:
-            Logger.Log("Shader error: attribute not found: Vertex_position")
-        
-        location = glGetAttribLocation(self._id, "Vertex_normal")
-        if not location in [None,-1]:
-                self._vertex_normal_attr_location = location
-        else:
-            Logger.Log("Shader error: attribute not found: Vertex_normal" )
-        
-        location = glGetAttribLocation(self._id, "Vertex_texture_coordinate")
-        if not location in [None,-1]:
-                self._vertex_texture_coord_attr_location = location
-        else:
-            Logger.Log("Shader error: attribute not found: Vertex_texture_coordinate" )    
-    
     def on_frame_start(self):
         # Send the light data through to the Shader Video Memory
         self.use()
-        
+
         # Set the Global ambient value
-        u_loc = self._uniform_locations['Global_ambient']
-        if self._global_ambient not in [None, -1]:
-            with self._global_ambient as ga:
-                glUniform4f(u_loc,ga.r,ga.g,ga.b,ga.a)
+        self.set_uniform_data('Global_ambient', self._global_ambient)
+        
         self.disable()
     
     def render(self, material, mesh):
-        if 'material.ambient' in self._uniform_locations:
-            u_loc = self._uniform_locations['material.ambient']
-            if u_loc not in [None, -1]:
-                with material.ambient as ma:
-                    glUniform4f(u_loc,ma.r,ma.g,ma.b,ma.a)
-        
-        if 'material.diffuse' in self._uniform_locations:
-            u_loc = self._uniform_locations['material.diffuse']
-            if u_loc not in [None, -1]:
-                with material.diffuse as md:
-                    glUniform4f(u_loc,md.r,md.g,md.b,md.a)
-                    
-        if 'material.specular' in self._uniform_locations:
-            u_loc = self._uniform_locations['material.specular']
-            if u_loc not in [None, -1]:
-                with material.specular as ms:
-                    glUniform4f(u_loc,ms.r,ms.g,ms.b,ms.a)
-                    
-        if 'material.shininess' in self._uniform_locations:
-            u_loc = self._uniform_locations['material.shininess']
-            if u_loc not in [None, -1]:
-                glUniform1f(u_loc,material.shininess)
-            
-        if not self._vertex_position_attr_location is None and \
-           not self._vertex_normal_attr_location is None and \
-           not self._vertex_texture_coord_attr_location is None:
-            glEnableVertexAttribArray(self._vertex_position_attr_location)
-            glEnableVertexAttribArray(self._vertex_normal_attr_location)
-            glEnableVertexAttribArray(self._vertex_texture_coord_attr_location)
+        self.set_uniform_data('material.ambient', material.ambient)
+        self.set_uniform_data('material.diffuse', material.diffuse)
+        self.set_uniform_data('material.specular', material.specular)
+        self.set_uniform_data('material.shininess', material.shininess)
+
+        if self.attributes_valid():
+            self.enable_attribute_arrays()
         
         if mesh.vertex_buffer.is_setup:
             with mesh.vertex_buffer as vb:
-                if not self._vertex_position_attr_location is None and \
-                   not self._vertex_normal_attr_location is None and \
-                   not self._vertex_texture_coord_attr_location is None:
-                    glVertexAttribPointer(self._vertex_position_attr_location, *vb.GetVertexAttribute())
-                    glVertexAttribPointer(self._vertex_normal_attr_location, *vb.GetNormalAttribute())
-                    glVertexAttribPointer(self._vertex_texture_coord_attr_location, *vb.GetTexCoordAttribute())
+                if self.attributes_valid():
+                    glVertexAttribPointer(self.get_attribute_location("Vertex_position"), *vb.GetVertexAttribute())
+                    glVertexAttribPointer(self.get_attribute_location("Vertex_normal"), *vb.GetNormalAttribute())
+                    glVertexAttribPointer(self.get_attribute_location("Vertex_texture_coordinate"), *vb.GetTexCoordAttribute())
                 
                 glDrawElements(GL_TRIANGLES, vb.count, GL_UNSIGNED_SHORT, vb.indices)
-                
-                if not self._vertex_position_attr_location is None and \
-                   not self._vertex_normal_attr_location is None and \
-                   not self._vertex_texture_coord_attr_location is None:
-                    glDisableVertexAttribArray(self._vertex_position_attr_location)
-                    glDisableVertexAttribArray(self._vertex_normal_attr_location)
-                    glDisableVertexAttribArray(self._vertex_texture_coord_attr_location)
+        
+        if self.attributes_valid():
+            self.disable_attribute_arrays()
